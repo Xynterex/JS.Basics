e43c6a95cee0e6ceb70e99000872e745f02866e9
Xynterex <158581153+Xynterex@users.noreply.github.com>
2024-07-10 09:24:29 +0800

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,1 +1,69 @@
+let myName = "Mr Lam";
+const myAge = 18;
+let myPhone = 1234567;
+myPhone = 76654321;
+let myArr = [1, 2, 3, 4, 5, 6]
+let isHuman = true;
 
+console.log(myName);
+console.log(myAge);
+console.log(myPhone);
+console.log(myArr);
+console.log(isHuman);
+
+// Playing with numbers
+let a = (5 + 7) * 2;
+let b = (5 - 7) / 3;
+let c = 5 ** 7;
+let d = 123 % 45;
+console.log(a, b, c, d);
+
+let e = Math.random();
+let f = Math.round(123,456789);
+let g = Math.cos(Math.PI);
+let h = Math.E ** 2;
+console.log(e, f, g, h);
+
+
+// Playing with strings
+let firstName = "Andrew";
+let lastName = "Tay";
+let fullName = firstName + " " + lastName;
+let nameLength = fullName.length;
+console.log(fullName + ", Length = " + nameLength);
+
+let language = "JavaScript";
+console.log(language[0]);
+console.log(language.substring(2, 5));
+console.log(language.toUpperCase());
+
+// if else
+let age = 20;
+
+if (age <= 12) {
+    console.log("Hello kid!");
+}
+else if (age <= 18) {
+    console.log("Hello teen!");
+}
+else {
+    console.log("Hello adult");
+}
+
+// For loop
+let fruits = ["orange", "apple", "pear", "watermelon"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+let counter = 10;
+while (counter > 0) {
+    console.log(counter);
+    counter--;
+}
+console.log("Happy New Year!")
